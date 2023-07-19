ac53cbcfa5832d32c6d67cfc62e136ccd66701a5
puxu <645560511@qq.com>
2023-07-19 09:46:05 +0800

    管理员入群版本

--- MPManager/Models/EncryptedPhoneNumModel.cs
+++ MPManager/Models/EncryptedPhoneNumModel.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PXLibCore.Attributes.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace WechatSDKCore.MPManager.Models
 {
@@ -14,18 +12,23 @@ namespace WechatSDKCore.MPManager.Models
         public string NickName { get; set; }
         [Required(ErrorMessage = "头像不能为空")]
         public string AvatarUrl { get; set; }
+        [IsTel]
+        public string Tel { get; set; }
+    }
+    public class MPLoginInptDto
+    {
+        [Required(ErrorMessage = "code不能为空")]
+        public string code { get; set; }
     }
     /// <summary>
     /// 微信小程序获取手机信息结构 
     /// </summary>
-    public class EncryptedPhoneNumModel
+    public class EncryptedPhoneNumModel : MPLoginInptDto
     {
-        [Required(ErrorMessage = "jscode不能为空")]
-        public string code { get; set; }
         public string encryptedData { get; set; }
         public string iv { get; set; }
     }
-    public class BindingEncryptedPhoneNumModel: EncryptedPhoneNumModel
+    public class BindingEncryptedPhoneNumModel : EncryptedPhoneNumModel
     {
         public bool IsBingding { get; set; }
     }
